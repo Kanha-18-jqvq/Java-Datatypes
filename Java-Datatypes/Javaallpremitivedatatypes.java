923897ea10a8f73106566be9fa1e7e7d564b4ed3
Kanha-18-jqvq <biswaranjandas7108@gmail.com>
2026-05-21 10:43:42 +0530

    Added datatypes folder and Primitive.java

---
+++ Java-Datatypes/Javaallpremitivedatatypes.java
@@ -0,0 +1,20 @@
+class Javaallpremitivedatatypes
+{
+    public static void main(String[] args) {
+        int a=90;
+        boolean b=false;
+        char c=78;
+        short s=908;
+        long l=97l;
+        float f=0.9f;
+        double d=89.89;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+
+    }
+}
